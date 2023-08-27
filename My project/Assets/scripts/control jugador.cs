29326c0b9ebad6553d05a3c5ed04e3f2d4a3c24a
Sebastian Carrero <mugssebastian@gmail.com>
2023-08-27 11:38:10 -0500

    dia dos

--- My project/Assets/scripts/control jugador.cs
+++ My project/Assets/scripts/control jugador.cs
@@ -14,6 +14,7 @@ public class controljugador : MonoBehaviour
     void Update()
     {
         //mover el tanke
-        transform.Translate(0, 0, 1);
+      //  transform.Translate(0, 0, 1);
+      transform.Translate(Vector3.forward * Time.deltaTime * 15);
     }
 }
